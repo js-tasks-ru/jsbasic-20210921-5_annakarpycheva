f269ab87166b1c7a4374f1fa7644ca8e9a721ae4
akarpych <anna.karpycheva@t-systems.com>
2021-09-30 20:39:54 +0300

    objects

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
 function sumSalary(salaries) {
-  // ваш код...
+let sum =  salaries.Ann + salaries.John + salaries.Pete;
+if (sum>0)
+  return sum
+   else return 0;
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,11 @@
-function isEmpty(obj) {
-  // ваш код...
+function isEmpty(schedule) {
+ for (var prop in schedule){
+  return false;
+  }
+  return true;
 }
+
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "подъём";
+alert( isEmpty(schedule) ); // false
